30383ae1d95879fe3b027e403639f2d4a2afd407
Tofique Ahmed  Khan <tofique@webkorps.com>
2022-04-15 19:26:43 +0530

    many to one using annotation

--- src/myproject/manytoone/entity/Branch.java
+++ src/myproject/manytoone/entity/Branch.java
@@ -1,8 +1,18 @@
 package myproject.manytoone.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "br2")
 public class Branch {
 
+	@Id
+	@Column(length = 5)
 	private String bid;
+	@Column(length = 10)
 	private String bname;
 	public String getBid() {
 		return bid;
--- src/myproject/manytoone/entity/Student.java
+++ src/myproject/manytoone/entity/Student.java
@@ -1,10 +1,24 @@
 package myproject.manytoone.entity;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "std2")
 public class Student {
 
+	@Id
+	@Column(length = 5)
 	private String sid;
+	@Column(length = 10)
 	private String sname;
+	@Column(length = 10)
 	private String saddr;
+	@ManyToOne(cascade = CascadeType.ALL)
 	private Branch branch;
 	public String getSid() {
 		return sid;
